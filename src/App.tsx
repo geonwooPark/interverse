153ca202ea2cd76c58ed7e65b098b6407a7ecfee
geonwooPark <white0581@naver.com>
2024-02-26 13:36:41 +0900

    Chore: 프로젝트 초기 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-red-400 underline">InterBus</h1>
+      <h1 className="text-4xl font-bold text-red-400 underline">InterVerse</h1>
+    </>
+  )
+}
+
+export default App
