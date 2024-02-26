d54d206d7ae2dbab81fbd424ebb7283b424569dc
geonwooPark <white0581@naver.com>
2024-02-26 19:33:34 +0900

    Chore: Font 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <>
+    <div className="font-neodgm">
       <h1 className="text-4xl font-bold text-red-400 underline">InterBus</h1>
       <h1 className="text-4xl font-bold text-red-400 underline">InterVerse</h1>
-    </>
+    </div>
   )
 }
 
