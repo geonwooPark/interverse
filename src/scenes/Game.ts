0a5943a0bb70114e381f6e6f12f54bc7c564b054
박건우 <white0581@naver.com>
2024-02-27 11:57:40 +0900

    Chore: 캐릭터 이동 테스트

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,5 +1,6 @@
 export default class Game extends Phaser.Scene {
   private map!: Phaser.Tilemaps.Tilemap
+  private player: any
 
   constructor() {
     // Scene Key
@@ -31,6 +32,11 @@ export default class Game extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32,
     })
+
+    this.load.spritesheet('adam', 'assets/character/adam.png', {
+      frameWidth: 48,
+      frameHeight: 96,
+    })
   }
 
   // Scene이 로드될 때 한번 호출, 게임 오브젝트 배치
@@ -40,12 +46,23 @@ export default class Game extends Phaser.Scene {
     // 타일셋 이미지를 로드하여 타일맵에 추가
     const FloorAndWall = this.map.addTilesetImage('floorAndWall', 'tiles_wall')
     // 레이어 추가
-    const floorLayer = this.map.createLayer('Floor', FloorAndWall)
+    const floorLayer = this.map.createLayer('Ground', FloorAndWall)
     // 타일맵 레이어에서 특정 속성을 가진 타일들에 대해 충돌처리 활성화 (collides 속성을 가진 모들 타일에 충돌 활성화)
     floorLayer?.setCollisionByProperty({ collides: true })
+
+    this.player = this.physics.add.sprite(100, 100, 'adam')
   }
 
-  launchGame() {
-    this.scene.launch('game')
+  update() {
+    const cursorsKeys = this.input.keyboard?.createCursorKeys()
+
+    if (cursorsKeys?.left.isDown) {
+      console.log('왼쪽 이동')
+      this.player.setVelocityX(-160)
+    }
+    if (cursorsKeys?.right.isDown) {
+      console.log('왼쪽 이동')
+      this.player.setVelocityX(160)
+    }
   }
 }
