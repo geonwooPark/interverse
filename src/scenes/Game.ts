cadf872c81ce6a4bf02da07bf092cc012d7097af
geonwooPark <white0581@naver.com>
2024-02-27 21:51:17 +0900

    Feat: 이미지 레이어 작업

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -11,25 +11,33 @@ export default class Game extends Phaser.Scene {
   // Scene이 로드되기 전에 호출, 사용할 에셋을 로드
   preload() {
     // Tiled로 생성된 JSON 형식의 타입맵을 로드
-    this.load.tilemapTiledJSON('tilemap', 'assets/map/map.json')
+    this.load.tilemapTiledJSON('tilemap', 'assets/map/interverse.json')
     // 스프라이트 시트 이미지 파일을 로드
-    this.load.spritesheet('tiles_wall', 'assets/tile/floorAndWall.png', {
+    this.load.spritesheet('floorAndWall', 'assets/builder/floorAndWall.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
-    this.load.spritesheet('chair', 'assets/object/chair.png', {
-      frameWidth: 32,
-      frameHeight: 64,
-    })
-    this.load.spritesheet('secretary', 'assets/object/secretary.png', {
-      frameWidth: 64,
-      frameHeight: 32,
-    })
-    this.load.spritesheet('interior', 'assets/tile/interior.png', {
+    this.load.spritesheet('office', 'assets/builder/office.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
-    this.load.spritesheet('office', 'assets/tile/office.png', {
+    this.load.spritesheet('classroom', 'assets/builder/classroom.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
+    this.load.spritesheet('conference', 'assets/builder/conference.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
+    this.load.spritesheet('livingRoom', 'assets/builder/livingRoom.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
+    this.load.spritesheet('bedroom', 'assets/builder/bedroom.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
+    this.load.spritesheet('halloween', 'assets/builder/halloween.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
@@ -45,18 +53,76 @@ export default class Game extends Phaser.Scene {
     // 타일맵 로드
     this.map = this.make.tilemap({ key: 'tilemap' })
     // 타일셋 이미지를 로드하여 타일맵에 추가
-    const FloorAndWall = this.map.addTilesetImage('floorAndWall', 'tiles_wall')
+    const FloorAndWall = this.map.addTilesetImage(
+      'floorAndWall',
+      'floorAndWall',
+    )
+    const Office = this.map.addTilesetImage('office', 'office')
+    const LivingRoom = this.map.addTilesetImage('livingRoom', 'livingRoom')
+    const Classroom = this.map.addTilesetImage('classroom', 'classroom')
+    const Bedroom = this.map.addTilesetImage('bedroom', 'bedroom')
+    const Halloween = this.map.addTilesetImage('halloween', 'halloween')
+    const Conference = this.map.addTilesetImage('conference', 'conference')
+
     // 레이어 추가
-    const floorLayer = this.map.createLayer('Ground', FloorAndWall)
-    // 타일맵 레이어에서 특정 속성을 가진 타일들에 대해 충돌처리 활성화 (collides 속성을 가진 모들 타일에 충돌 활성화)
-    this.map.setCollisionByProperty({ collides: true })
-    floorLayer?.setCollisionByProperty({ collides: true })
+    this.map.createLayer('Ground', FloorAndWall!)
+    const wallLayer = this.map.createLayer('Wall', FloorAndWall!)
+    const interiorLayer = this.map.createLayer('Interior', [
+      Office!,
+      LivingRoom!,
+      Classroom!,
+      Bedroom!,
+    ])
+    const itemLayer = this.map.createLayer('Item', [Office!, Halloween!])
 
-    this.player = this.physics.add.sprite(100, 100, 'adam')
-    this.player.setBounce(0.2)
-    this.player.setCollideWorldBounds(true)
+    // 타일맵 레이어에서 특정 속성을 가진 타일들에 대해 충돌처리 활성화 (collide 속성을 가진 모들 타일에 충돌 활성화)
+    wallLayer?.setCollisionByProperty({ collide: true })
 
-    this.physics.add.collider(floorLayer!, this.player)
+    // 아바타 생성
+    this.player = this.physics.add.sprite(200, 200, 'adam')
+
+    // // 의자 관련
+    // const chairs = this.physics.add.staticGroup()
+    // // getObjectLayer - Tiled 맵에서 오브젝트 레이어를 가져오는 메서드
+    // const chairLayer = this.map.getObjectLayer('Chair')
+    // // chair레이어의 오브젝트를 반복하여 방향 속성을 가져옴
+    // chairLayer?.objects.forEach((chairObj) => {
+    //   const item = this.addObjectFromTiled(chairs, chairObj, 'chair', 'chair')
+    //   item.itemDirection = chairObj.properties[0].value
+
+    //   console.log(item)
+    // })
+
+    // 비서 관련
+    const secretary = this.physics.add.staticGroup()
+    // getObjectLayer - Tiled 맵에서 오브젝트 레이어를 가져오는 메서드
+    const secretaryLayer = this.map.getObjectLayer('Secretary')
+    secretaryLayer?.objects.forEach((secretaryObj) => {
+      const item = this.addObjectFromTiled(
+        secretary,
+        secretaryObj,
+        'secretary',
+        'secretary',
+      )
+    })
+
+    //
+    this.physics.add.collider(wallLayer!, this.player)
+  }
+
+  private addObjectFromTiled(
+    group: Phaser.Physics.Arcade.StaticGroup,
+    object: Phaser.Types.Tilemaps.TiledObject,
+    key: string,
+    tilesetName: string,
+  ) {
+    const firstgid = this.map.getTileset(tilesetName)?.firstgid
+    const actualX = object.x! + object.width! * 0.5
+    const actualY = object.y! - object.height! * 0.5
+    const obj = group
+      .get(actualX, actualY, key, object.gid! - firstgid!)
+      .setDepth(actualY)
+    return obj
   }
 
   // 주로 게임 상태를 업데이트하고 게임 객체들의 상태를 조작하는 데 사용. 게임이 실행되는 동안 지속적으로 호출됨
@@ -67,7 +133,6 @@ export default class Game extends Phaser.Scene {
 
     // 화살표 키 입력 감지
     if (cursorsKeys?.left.isDown) vx -= this.moveSpeed
-
     if (cursorsKeys?.right.isDown) vx += this.moveSpeed
     if (cursorsKeys?.up.isDown) vy -= this.moveSpeed
     if (cursorsKeys?.down.isDown) vy += this.moveSpeed
