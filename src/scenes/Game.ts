491c95a40655a14f8a40a9df0145001297b4d4af
geonwooPark <white0581@naver.com>
2024-02-28 14:27:02 +0900

    Feat: Layer 작업

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -27,19 +27,7 @@ export default class Game extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32,
     })
-    this.load.spritesheet('conferenceHall', 'assets/builder/conference.png', {
-      frameWidth: 32,
-      frameHeight: 32,
-    })
-    this.load.spritesheet('livingRoom', 'assets/builder/livingRoom.png', {
-      frameWidth: 32,
-      frameHeight: 32,
-    })
-    this.load.spritesheet('bedroom', 'assets/builder/bedroom.png', {
-      frameWidth: 32,
-      frameHeight: 32,
-    })
-    this.load.spritesheet('halloween', 'assets/builder/halloween.png', {
+    this.load.spritesheet('chair', 'assets/builder/chair.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
@@ -64,24 +52,16 @@ export default class Game extends Phaser.Scene {
       'floorAndWall',
     )
     const Office = this.map.addTilesetImage('office', 'office')
-    const LivingRoom = this.map.addTilesetImage('livingRoom', 'livingRoom')
     const Classroom = this.map.addTilesetImage('classroom', 'classroom')
-    const Bedroom = this.map.addTilesetImage('bedroom', 'bedroom')
-    const Halloween = this.map.addTilesetImage('halloween', 'halloween')
-    const ConferenceHall = this.map.addTilesetImage(
-      'conferenceHall',
-      'conferenceHall',
-    )
-
+    // Ground Layer
     this.map.createLayer('Ground', FloorAndWall!)
+    // Wall Layer
     const wallLayer = this.map.createLayer('Wall', FloorAndWall!)
 
-    // 비서
+    // Secretary Layer
     const secretary = this.physics.add.staticGroup()
-    // getObjectLayer - Tiled 맵에서 오브젝트 레이어를 가져오는 메서드
     const secretaryLayer = this.map.getObjectLayer('Secretary')
     secretaryLayer?.objects.forEach((object) => {
-      console.log(1)
       const firstgid = this.map.getTileset('classroom')?.firstgid
       const actualX = object.x! + object.width! * 0.5
       const actualY = object.y! - object.height! * 0.5
@@ -94,9 +74,47 @@ export default class Game extends Phaser.Scene {
       return obj
     })
 
-    // 책상
+    // ChairToDown Layer
+    const chairToDown = this.physics.add.staticGroup()
+    const chairToDownLayer = this.map.getObjectLayer('ChairToDown')
+    chairToDownLayer?.objects.forEach((object) => {
+      const firstgid = this.map.getTileset('chair')?.firstgid
+      const actualX = object.x! + object.width! * 0.5
+      const actualY = object.y! - object.height! * 0.5
+      const obj = chairToDown.get(
+        actualX,
+        actualY,
+        'chair',
+        object.gid! - firstgid!,
+      )
+      return obj
+    })
+
+    // interior Layer
+    const interiorLayer = this.map.createLayer('Interior', [
+      Office!,
+      Classroom!,
+    ])
+
+    // interiorOnCollide Layer
+    const interiorOnCollide = this.physics.add.staticGroup()
+    const interiorOnCollideLayer = this.map.getObjectLayer('InteriorOnCollide')
+    interiorOnCollideLayer?.objects.forEach((object) => {
+      const firstgid = this.map.getTileset('office')?.firstgid
+      const actualX = object.x! + object.width! * 0.5
+      const actualY = object.y! - object.height! * 0.5
+      const obj = interiorOnCollide.get(
+        actualX,
+        actualY,
+        'office',
+        object.gid! - firstgid!,
+      )
+      return obj
+    })
+
+    // Table Layer
     const table = this.physics.add.staticGroup()
-    const tableLayer = this.map.getObjectLayer('table')
+    const tableLayer = this.map.getObjectLayer('Table')
     tableLayer?.objects.forEach((object) => {
       const firstgid = this.map.getTileset('office')?.firstgid
       const actualX = object.x! + object.width! * 0.5
@@ -105,41 +123,39 @@ export default class Game extends Phaser.Scene {
       return obj
     })
 
-    // 의자
-    const chairs = this.physics.add.staticGroup()
-    // getObjectLayer - Tiled 맵에서 오브젝트 레이어를 가져오는 메서드
-    const chairLayer = this.map.getObjectLayer('ChairToDown')
-    chairLayer?.objects.forEach((object) => {
-      const firstgid = this.map.getTileset('office')?.firstgid
-      const actualX = object.x! + object.width! * 0.5
-      const actualY = object.y! - object.height! * 0.5
-      const obj = chairs.get(
-        actualX,
-        actualY,
-        'office',
-        object.gid! - firstgid!,
-      )
-    })
-
-    const interiorLayer = this.map.createLayer('Interior', [
-      Office!,
-      LivingRoom!,
-      Classroom!,
-      Bedroom!,
-    ])
-    const itemLayer = this.map.createLayer('Item', [Office!, Halloween!])
-
-    // 아바타 생성
+    // 아바타 Layer
     this.player = this.physics.add.sprite(200, 200, 'conference')
     // 애니메이션 추가
     createAvatarAnims(this.anims)
     // 첫 랜더링시 플레이어 애니메이션 적용
     this.player.anims.play('conference_stand_down', true)
     this.physics.add.collider(wallLayer!, this.player)
+    this.player.setSize(32, 32)
 
-    // 플레이어와 비서 간의 충돌처리
+    // interiorTop Layer
+    const interiorTopLayer = this.map.createLayer('InteriorTop', Office!)
+
+    // chairToUp Layer
+    const chairToUp = this.physics.add.staticGroup()
+    const chairToUpLayer = this.map.getObjectLayer('ChairToUp')
+    chairToUpLayer?.objects.forEach((object) => {
+      const firstgid = this.map.getTileset('chair')?.firstgid
+      const actualX = object.x! + object.width! * 0.5
+      const actualY = object.y! - object.height! * 0.5
+      const obj = chairToUp.get(
+        actualX,
+        actualY,
+        'chair',
+        object.gid! - firstgid!,
+      )
+      return obj
+    })
+
+    // 플레이어와 물체 간의 충돌처리
     if (this.player) {
       this.physics.add.collider(this.player, secretary)
+      this.physics.add.collider(this.player, table)
+      this.physics.add.collider(this.player, interiorOnCollide)
     }
 
     // 타일맵 레이어에서 특정 속성을 가진 타일들에 대해 충돌처리 활성화 (collide 속성을 가진 모들 타일에 충돌 활성화)
