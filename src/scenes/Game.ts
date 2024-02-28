0326981cc53789bbb883e4a59f2ce258bc0d13be
kangkyeongseo <kks_big@naver.com>
2024-02-28 14:34:39 +0900

    Feat: Player 클래스 생성

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,9 +1,9 @@
 import { createAvatarAnims } from '../anims/AvatarAnims'
+import Player from '../avatars/Player'
 
 export default class Game extends Phaser.Scene {
   private map!: Phaser.Tilemaps.Tilemap
-  private player: any
-  cursors: any
+  player: any
 
   constructor() {
     // Scene Key
@@ -112,12 +112,11 @@ export default class Game extends Phaser.Scene {
     // 타일맵 레이어에서 특정 속성을 가진 타일들에 대해 충돌처리 활성화 (collide 속성을 가진 모들 타일에 충돌 활성화)
     wallLayer?.setCollisionByProperty({ collide: true })
 
-    // 아바타 생성
-    this.player = this.physics.add.sprite(100, 100, 'conference')
     // 애니메이션 추가
     createAvatarAnims(this.anims)
-    // 첫 랜더링시 플레이어 애니메이션 적용
-    this.player.anims.play('conference_stand_down', true)
+    // 아바타 생성
+    this.player = new Player(this, 500, 150, 'conference')
+    this.player.setNickname('player')
     this.physics.add.collider(wallLayer!, this.player)
   }
 
@@ -138,31 +137,7 @@ export default class Game extends Phaser.Scene {
 
   // 주로 게임 상태를 업데이트하고 게임 객체들의 상태를 조작하는 데 사용. 게임이 실행되는 동안 지속적으로 호출됨
   update() {
-    this.cursors = this.input.keyboard?.createCursorKeys()
     const cursorsKeys = this.input.keyboard?.createCursorKeys()
-    const moveSpeed = 200
-    let vx = 0
-    let vy = 0
-
-    // 화살표 키 입력 감지 및 애니메이션 실행
-    if (cursorsKeys?.left.isDown) {
-      vx -= moveSpeed
-      this.player.anims.play('conference_run_left', true)
-    } else if (cursorsKeys?.right.isDown) {
-      vx += moveSpeed
-      this.player.anims.play('conference_run_right', true)
-    } else if (cursorsKeys?.up.isDown) {
-      vy -= moveSpeed
-      this.player.anims.play('conference_run_up', true)
-    } else if (cursorsKeys?.down.isDown) {
-      vy += moveSpeed
-      this.player.anims.play('conference_run_down', true)
-    } else {
-      // 이동 후 정지시 애니메이션 실행
-      const animParts = this.player.anims.currentAnim.key.split('_')
-      animParts[1] = 'stand'
-      this.player.anims.play(animParts.join('_'), true)
-    }
-    this.player.setVelocity(vx, vy)
+    this.player.update(cursorsKeys)
   }
 }
