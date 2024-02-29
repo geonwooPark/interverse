301e27fbfe90358fc20f2f9e6bc7adaec7597a1d
kangkyeongseo <kks_big@naver.com>
2024-02-29 18:17:38 +0900

    Feat: 플레이어 의자 앉기 기능

--- client/src/items/item.ts
+++ client/src/items/item.ts
@@ -1,6 +1,6 @@
 export default class Item extends Phaser.Physics.Arcade.Sprite {
-  interacrionBox: Phaser.GameObjects.Container
-  itemType!: 'secretary'
+  private interacrionBox: Phaser.GameObjects.Container
+  itemType!: 'secretary' | 'chair'
 
   constructor(
     scene: Phaser.Scene,
@@ -10,24 +10,25 @@ export default class Item extends Phaser.Physics.Arcade.Sprite {
     frame?: string | number,
   ) {
     super(scene, x, y, texture, frame)
-
+    // 텍스트를 담을 박스입니다.
     this.interacrionBox = this.scene.add.container().setDepth(2000)
   }
-
+  // 텍스트 박스 출력
   setInteractionBox(content: string) {
+    // 텍스트 생성
     const innerContent = this.scene.add
       .text(0, 0, content)
       .setFontSize(12)
       .setColor('#000000')
-
+    // 텍스트 박스 크기 및 위치 계산
     const innerContentWidth = innerContent.width
     const innerContentHeight = innerContent.height
 
     const interacrionBoxWidth = innerContentWidth + 8
     const interacrionBoxHeight = innerContentHeight + 4
-    const interacrionBoxX = this.x
-    const interacrionBoxY = this.y
-
+    const interacrionBoxX = this.x - interacrionBoxWidth * 0.5
+    const interacrionBoxY = this.y + interacrionBoxHeight * 0.5
+    // 텍스트 박스 생성
     this.interacrionBox.add(
       this.scene.add
         .graphics()
@@ -40,11 +41,12 @@ export default class Item extends Phaser.Physics.Arcade.Sprite {
           5,
         ),
     )
+    // 텍스트 박스에 텍스트를 담습니다.
     this.interacrionBox.add(
-      innerContent.setPosition(interacrionBoxX, interacrionBoxY),
+      innerContent.setPosition(interacrionBoxX + 4, interacrionBoxY + 2),
     )
   }
-
+  // 텍스트 박스 삭제
   clearInteractionBox() {
     this.interacrionBox.removeAll(true)
   }
