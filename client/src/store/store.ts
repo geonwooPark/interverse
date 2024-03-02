e1b6ebe89a2460188da47516545d72dbd180d07c
kangkyeongseo <kks_big@naver.com>
2024-03-02 19:05:54 +0900

    Feat: Alert 상태 전역 관리

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { ModalDisplaySlice } from './features/modalDisplaySlice'
 import { ModalContentSlice } from './features/modalContentSlice'
+import { AlertSlice } from './features/alertSlice'
 
 export const store = configureStore({
   reducer: {
     modalDisplay: ModalDisplaySlice.reducer,
     modalContent: ModalContentSlice.reducer,
+    alert: AlertSlice.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
