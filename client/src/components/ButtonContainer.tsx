5e88d50338bb617147618cce6a8c191bd59f331b
kangkyeongseo <kks_big@naver.com>
2024-03-05 19:13:49 +0900

    Feat: 링크 공유하기 기능

--- client/src/components/ButtonContainer.tsx
+++ client/src/components/ButtonContainer.tsx
@@ -1,3 +1,8 @@
+import {
+  changeAlertContent,
+  closeAlert,
+  openAlert,
+} from '../store/features/alertSlice'
 import { changeModalContent } from '../store/features/modalContentSlice'
 import { closeModal, openModal } from '../store/features/modalDisplaySlice'
 import { useAppDispatch } from '../store/store'
@@ -6,6 +11,15 @@ import IconOff from '../svgs/IconOff.svg?react'
 
 function ButtonContainer() {
   const dispatch = useAppDispatch()
+
+  const onLinkClick = () => {
+    dispatch(changeAlertContent('복사 완료! 링크를 공유하여 초대하세요.'))
+    dispatch(openAlert())
+    navigator.clipboard.writeText(window.location.href)
+    setTimeout(() => {
+      dispatch(closeAlert())
+    }, 5000)
+  }
 
   const onOffClick = () => {
     dispatch(
@@ -24,7 +38,10 @@ function ButtonContainer() {
 
   return (
     <div className="fixed right-8 top-4 flex gap-3">
-      <button className="flex size-[50px] items-center justify-center rounded-full bg-purple-600 text-white duration-200 hover:bg-purple-700">
+      <button
+        onClick={onLinkClick}
+        className="flex size-[50px] items-center justify-center rounded-full bg-purple-600 text-white duration-200 hover:bg-purple-700"
+      >
         <IconLink />
       </button>
       <button
