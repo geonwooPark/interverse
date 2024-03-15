01c8dbe3506a0acb531c52e7affb27c403e9be9e
geonwooPark <white0581@naver.com>
2024-03-15 17:25:45 +0900

    Chore: 배포

--- server/index.ts
+++ server/index.ts
@@ -11,16 +11,14 @@ import path from 'path'
 const app = express()
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
 })
-
 const server = http.createServer(app)
 
 const io = new Server<ClientToServerEvents, ServerToClientEvents>(server, {
   cors: {
-    origin: [`team94-interverse.vercel.app`],
+    origin: [`https://team94-interverse.vercel.app`],
     methods: ['GET', 'POST'],
     credentials: true,
   },
