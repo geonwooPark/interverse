b882d2746d9a5946b29e10aafb39464f4f2162c0
kangkyeongseo <kks_big@naver.com>
2024-03-22 15:40:00 +0900

    Style: Manual 카드 이미지 변경

--- client/src/components/Modals/ManualModal/ManualCard.tsx
+++ client/src/components/Modals/ManualModal/ManualCard.tsx
@@ -1,6 +1,6 @@
 function ManualCard() {
   return (
-    <div className="mx-auto flex w-[80%] max-w-[1200px] items-center justify-between ">
+    <div className="mx-auto flex w-[85%] max-w-[2000px] items-center justify-between ">
       <img src="/images/manual.png" />
     </div>
   )
