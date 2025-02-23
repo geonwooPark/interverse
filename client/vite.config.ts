6567b37ef568b3cf38b474d06d04a5853db72fd1
박건우 <white0581@naver.com>
2025-02-23 17:38:08 +0900

    Chore: vite 절대경로 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,22 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: [
+      { find: '@components', replacement: '/src/components' },
+      { find: '@constants', replacement: '/src/constants' },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@games', replacement: '/src/games' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@layouts', replacement: '/src/layouts' },
+      { find: '@lib', replacement: '/src/lib' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@providers', replacement: '/src/providers' },
+      { find: '@routes', replacement: '/src/routes' },
+      { find: '@store', replacement: '/src/store' },
+      { find: '@utils', replacement: '/src/utils' },
+    ],
+  },
   css: {
     postcss: {
       plugins: [tailwindcss],
