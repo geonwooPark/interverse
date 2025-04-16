c101baa3b3088b1543d3034eef1cf3110009cb6b
geonwoo <geonwoo@geonwooui-Macmini.local>
2025-04-17 00:25:28 +0900

    Chore: tailwind config 설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,28 +1,28 @@
+import {
+  fontFamily,
+  screens,
+  typography,
+  backgroundColor,
+  zIndex,
+  colors,
+  boxShadow,
+} from './src/theme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
-      fontFamily: {
-        neodgm: ['neodgm'],
-      },
-      keyframes: {
-        myRoomBgKeyframes: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(calc(-100% + 360px))' },
-        },
-      },
-      animation: {
-        myRoomBg: 'myRoomBgKeyframes 5s linear infinite',
-      },
-      cursor: {
-        default: 'url(/assets/cursor/Point.png), default',
-        pointer: 'url(/assets/cursor/hand.png), pointer',
-      },
-      boxShadow: {
-        level1: '0px 2px 20px 2px rgba(0, 0, 0, 0.12)',
-      },
+      fontFamily,
+      screens,
+      fontSize: typography,
+      backgroundColor,
+      zIndex,
+      colors,
+      boxShadow,
     },
   },
+
   plugins: [],
 }
