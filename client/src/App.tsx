e9bae2fab418724228ebdb0323eb50e0904b11e3
박건우 <white0581@naver.com>
2025-04-26 20:26:37 +0900

    Chore: Tanstack Query 설치 및 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,7 @@
-import RoomsProvider from '@providers/RoomsProvider'
 import Router from './routes/index'
-import AuthProvider from '@providers/AuthProvider'
 
 function App() {
-  return (
-    <RoomsProvider>
-      <AuthProvider>
-        <Router />
-      </AuthProvider>
-    </RoomsProvider>
-  )
+  return <Router />
 }
 
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,18 @@ import App from './App.tsx'
 import './index.css'
 import './managers/GameManager.ts'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
   // </React.StrictMode>,
 )
